14a2c28e550042d50cdb4b1690558d352980aa86
upi1682 <upi538287@gmail.com>
2025-10-16 16:19:28 +0700

    1

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,24 @@
+#z konversi suhu
+def konversiSuhu(x, y, z) :
+    global hasil
+    #cek kondisi untuk celcius ke farenheit
+    if format == 'c' and z == 'f' :
+        f = (9/5) * x + 32
+        hasil = f'{f}{z.upper()}'
+    #cek kondisi untuk celcius ke kelvin
+    if format == 'c' and z == 'k' :
+        k = x + 273,15
+        hasil = f'{k}{z.upper()}'
+    if y == 'f' and z == 'c' :
+        c = (x - 32) * (5/9)
+        hasil = f'{c}{z.upper()}' 
+    if y == 'f' and z == 'k' :
+       k = (5/9) * x + 459.67
+       hasil = f'{k}{z.upper()}'
+    elif y == 'k' and z == 'c' :
+       c =  x - 273,15
+    elif y == 'f' and z == 'f' :
+        f = (x - 273,15) * (9/5) + 32
+        hasil = f'{f}{z.upper()}'
+    else :
+        hasil = "konversi nilai tidak valid"
